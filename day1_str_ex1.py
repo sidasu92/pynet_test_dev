8b37e85fab2337d987958af8a4674399a055649b
sid <sidhant.sa@gmail.com>
2019-06-20 13:45:55 -0700

    str_ex1 sol

---
+++ day1_str_ex1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python
+
+name1 = "Amar"
+name2 = "Akbar"
+name3 = "Anthony"
+name4 = input("Enter a name: ")
+
+print(f"{name1:>30}\n{name2:>30}\n{name3:>30}")
+print(f"{name4:>30}")
